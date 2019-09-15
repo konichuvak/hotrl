cdeaf40d049bdb107a24c748ed98734461c879ec
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-09-15 04:36:27 -0400

    feat: multiroom

--- run_sim_multiroom.py
+++ run_sim_multiroom.py
@@ -1,6 +1,6 @@
 import time
 import numpy as np
-from hotrl.envs.house import House, Homie
+from hotrl.envs.house import House, Homie, MultiRoomHouse
 from hotrl.envs.wrappers import FullyObsWrapper
 
 homies = [Homie(initial_room='Bedroom')]
@@ -11,20 +11,23 @@ temperatures = np.array([
     [-30, 25, 25, 25, -30],
     [-30, -30, -30, -30, -30]
 ], dtype=float)
-env = FullyObsWrapper(House(size=5, homies=homies, temperatures=temperatures))
+# env = FullyObsWrapper(House(size=5, homies=homies, temperatures=temperatures))
+env = FullyObsWrapper(MultiRoomHouse(homies=homies, seed=np.random.randint(0,100)))
 
 n_episodes = 10000
-action = np.zeros((5, 5))
+action = [0,1,4]
 for n in range(n_episodes):
     obs, reward, done, homie_info = env.step(action)
+    action = []
+    time.sleep(0.1)
     for homie, info in homie_info.items():
-        if info["temperature"] < info["comfort"][0]:
+        if info["temperature"] < info["comfort"][1]:
             print(f"Heating home in {info['room']}. "
                   f"Temperature is {info['temperature']}. "
                   f"Time is {info['dt']}")
-            for i in env.rooms[info['room']]:
-                action[i] = 1
-
+            if info['room'] == 'Outside':
+                continue
+            action.append(env.room_names.index(info['room']))
     object_matrix = obs[:, :, 0]
     temp_matrix = obs[:, :, 2]
     env.render(temperature=True)
